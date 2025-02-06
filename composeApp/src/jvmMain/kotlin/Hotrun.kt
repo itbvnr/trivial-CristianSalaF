01b36b16dfde8876f7f71170dfd659cd669e9e55
CristianSalaF <cristian.sala.7e8@itb.cat>
2025-02-06 10:38:29 +0100

    Updated Title in Hotrun.kt

--- composeApp/src/jvmMain/kotlin/Hotrun.kt
+++ composeApp/src/jvmMain/kotlin/Hotrun.kt
@@ -9,8 +9,8 @@ import org.jetbrains.compose.reload.DevelopmentEntryPoint
 
 fun main() = application {
     Window(
-        title = "M78Exercices",
-        state = rememberWindowState(width = 800.dp, height = 600.dp),
+        title = "TrivialApp-cs7e8",/*
+        state = rememberWindowState(width = 800.dp, height = 600.dp),*/
         onCloseRequest = ::exitApplication,
     ) {
         window.minimumSize = Dimension(350, 600)
