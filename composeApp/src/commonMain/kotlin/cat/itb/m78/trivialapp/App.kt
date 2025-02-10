5f2f31eefed8d663c41d7d0534023f52939048bb
CristianSalaF <cristian.sala.7e8@itb.cat>
2025-02-10 13:04:20 +0100

    Added Main menu basc layout

--- composeApp/src/commonMain/kotlin/cat/itb/m78/trivialapp/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/trivialapp/App.kt
@@ -1,16 +1,46 @@
 package cat.itb.m78.trivialapp
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import cat.itb.m78.trivialapp.theme.AppTheme
+import kotlinx.coroutines.launch
+import m78exercices.composeapp.generated.resources.Res
+import m78exercices.composeapp.generated.resources.icon
+import org.jetbrains.compose.resources.painterResource
+import kotlin.random.Random
 
 @Composable
 internal fun App() = AppTheme {
     Box(Modifier.fillMaxSize()){
-        Text("Your app goes here!", Modifier.align(Alignment.Center))
+        Column(modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            Image(
+                painter = painterResource(Res.drawable.icon),
+                modifier = Modifier.size(128.dp).padding(10.dp),
+                contentDescription = null)
+
+            Button(onClick = {
+                //button code
+            }){
+                Text("New Game")
+            }
+
+            Button(onClick = {
+                //button code
+            }){
+                Text("Settings")
+            }
+        }
     }
 }
