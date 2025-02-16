4404bacf7269320a2f9f5232f717014a3a925d74
CristianSalaF <cristian.sala.7e8@itb.cat>
2025-02-16 19:33:54 +0100

    Fixed Image code from testing. Images not recogniced at home, so //TODO in class
    Note: Hotrun and :composeApp runs, but ./gradlew build or clean doesn't. and also breaks the hotrun and :composeApp, they stop working after that even with undo.

--- composeApp/src/commonMain/kotlin/cat/itb/m78/trivialapp/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/trivialapp/App.kt
@@ -1,13 +1,17 @@
 package cat.itb.m78.trivialapp
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import m78exercices.composeapp.generated.resources.Res
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 internal fun App() {
@@ -16,7 +20,7 @@ internal fun App() {
 
     Box(modifier = Modifier.fillMaxSize()) {
         /*Image(
-            painter = painterResource("background.png"),
+            painter = painterResource(Res.drawable.icon),
             contentDescription = null,
             modifier = Modifier.fillMaxSize(),
             contentScale = ContentScale.FillBounds
